214f9645d00618037da29f0fb0cf5524dd848659
Alecto Irene Perez <perez.cs@pm.me>
2021-09-16 23:55:09 -0600

    Added ends_with 😌💕

--- include/catify.hpp
+++ include/catify.hpp
@@ -1,5 +1,13 @@
 #pragma once
+#include <string_view>
+#include <cstddef>
 
 namespace catify {
-
+constexpr size_t ends_with(std::string_view line, auto const& range) {
+    for (std::string_view ending : range) {
+        if (line.ends_with(ending))
+            return ending.size();
+    }
+    return 0;
+}
 }
