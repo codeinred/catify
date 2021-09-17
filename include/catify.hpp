d43339b08426b411eda30c7b0b9cce202a709391
Alecto Irene Perez <perez.cs@pm.me>
2021-09-16 23:57:49 -0600

    Cleaned + documented 💕💅

--- include/catify.hpp
+++ include/catify.hpp
@@ -1,9 +1,10 @@
 #pragma once
-#include <string_view>
 #include <cstddef>
+#include <string_view>
 
 namespace catify {
-constexpr size_t ends_with(std::string_view line, auto const& range) {
+// Returns the size of the first matching suffix. If none match, returns 0
+constexpr size_t check_suffix(std::string_view line, auto const& range) {
     for (std::string_view ending : range) {
         if (line.ends_with(ending))
             return ending.size();
@@ -14,9 +15,9 @@ constexpr size_t ends_with(std::string_view line, auto const& range) {
 // Removes suffixes in the range `endings` from line
 constexpr auto sanitize_ending(std::string_view line, auto const& endings)
     -> std::string_view {
-    while (size_t ending_len = ends_with(line, endings)) {
+    while (size_t ending_len = check_suffix(line, endings)) {
         line.remove_suffix(ending_len);
     }
     return line;
 }
-}
+} // namespace catify
