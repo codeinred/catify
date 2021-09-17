f383562961c193b0ce2bf0c5f78ea153a4311899
Alecto Irene Perez <perez.cs@pm.me>
2021-09-16 23:55:42 -0600

    Added sanitize_ending 😌💅

--- include/catify.hpp
+++ include/catify.hpp
@@ -10,4 +10,13 @@ constexpr size_t ends_with(std::string_view line, auto const& range) {
     }
     return 0;
 }
+
+// Removes suffixes in the range `endings` from line
+constexpr auto sanitize_ending(std::string_view line, auto const& endings)
+    -> std::string_view {
+    while (size_t ending_len = ends_with(line, endings)) {
+        line.remove_suffix(ending_len);
+    }
+    return line;
+}
 }
