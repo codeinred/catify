564e8a3a9f4f8b5334527cc60fe0e77637bd7967
Alecto Irene Perez <perez.cs@pm.me>
2021-09-19 15:30:25 -0600

    Updated check for compilers 💅😌

--- build.sh
+++ build.sh
@@ -3,12 +3,20 @@
 # This gets the current directory of build.sh, then moves into that directory
 cd "$(dirname "$0")"
 
-if which g++-11 2> /dev/null; then
-    compiler="g++-11"
-else
-    compiler="g++"
-fi
+compilers=(
+    "clang++-12"
+    "clang++"
+    "g++-11"
+    "g++"
+)
+
+for compiler in "${compilers[@]}"; do
+    if which "${compiler}" 2> /dev/null; then
+        cxx="${compiler}"
+        break
+    fi
+done
 
 # build project in $project_root/build
-cmake --preset="default" -DCMAKE_CXX_COMPILER="${compiler}" "${@}"
+cmake --preset="default" -DCMAKE_CXX_COMPILER="${cxx}" "${@}"
 cmake --build --preset="default"
