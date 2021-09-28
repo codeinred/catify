256d8d4724120deb20e7b5f742e6f62a61003dd3
Alecto Irene Perez <perez.cs@pm.me>
2021-09-28 01:42:26 -0600

    Added .clang-format 💕🐈
    This adds the .clang-format used for formatting code files to the
    project, and also applies it consistently to the project. Only a few
    minor changes were made.
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- include/constants.hpp
+++ include/constants.hpp
@@ -6,6 +6,6 @@ namespace catify {
 using std::string_view_literals::operator""sv;
 
 constexpr std::array emoji_endings = {"🐈"sv, "😌"sv, "💅"sv, "💕"sv, "💖"sv};
-constexpr std::array endings_to_sanitize = {
-    " "sv, "🐈"sv, "😌"sv, "💅"sv, "💕"sv, "💖"sv};
+constexpr std::array endings_to_sanitize =
+    {" "sv, "🐈"sv, "😌"sv, "💅"sv, "💕"sv, "💖"sv};
 } // namespace catify
